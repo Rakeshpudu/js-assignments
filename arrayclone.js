e6be9c446c72506abc29fb0d6fae97b8a5111c9a
Rakeshpudu <rakeshpudi123@gmail.com>
2023-07-29 00:00:47 +0530

    answer added

---
+++ arrayclone.js
@@ -0,0 +1,3 @@
+const array_Clone=(arr)=>arr.slice(0);
+console.log(array_Clone([1,2,4,0]));
+console.log(array_Clone([1,2,[4,0]]));
---
+++ index.js
@@ -0,0 +1,4 @@
+
+const is_array=(data)=>Array.isArray(data);
+console.log(is_array('w3resource'));
+console.log(is_array([1,3,'s',2]));
